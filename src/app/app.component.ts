45f68a875bd9525a3034e3c028d10cb336dc8583
Damian Szkudlarski <d.szkudlarski@dtpoland.com>
2021-06-09 15:27:25 +0200

    new msg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,5 +5,5 @@ import { Component } from '@angular/core';
   template: `<div>Hello {{value}}</div>`,
 })
 export class AppComponent {
-  value = 'Bolek i Lolek';
+  value = 'Bolek, Lolek i Tola ';
 }
